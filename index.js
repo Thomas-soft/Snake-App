d8194a0bac8194e3c07b9d171dd3c83e7be8c6f6
unknown <dev.thomas.soft+git@gmail.com>
2022-11-21 16:16:40 +0100

    Déplacement fonctionnel (sans queue)

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ class Snake
         this.length = 1;
         this.speed = 1;
         this.velocity = {x: 0, y: 0};
-        this.pos =
+        this.dataPos =
         [
-            {x: 0, y: 0}
+            {x: 15, y: 15}
         ];
+        this.headPos = {x: this.dataPos[0].x, y: this.dataPos[0].y};
         this.move =
         {
             up: function() { utils.up() },
@@ -18,28 +19,68 @@ class Snake
         }
     }
 
+    verify()
+    {
+        if (snake.headPos.x <= 0)
+            snake.headPos.x = gridArea.size.width;
+        else if (snake.headPos.x > gridArea.size.width)
+            snake.headPos.x = 0;
+        if (snake.headPos.y <= 0)
+            snake.headPos.y = gridArea.size.height;
+        else if (snake.headPos.y > gridArea.size.height)
+            snake.headPos.y = 0;
+    }
+
     updatePosition()
     {
-        
-        this.pos[0].x += this.velocity.x;
-        this.pos[0].y += this.velocity.y;
+        this.headPos = this.dataPos[0];
+        this.headPos.x += this.velocity.x;
+        this.headPos.y += this.velocity.y;
+
+        this.verify();
 
         for (let i = 0; i <= this.length-1; i++)
         {
             const snake = document.getElementById(`snake${i}`);
-            snake.style.gridColumn = this.pos[i].x;
-            snake.style.gridRow = this.pos[i].y;
+            snake.style.gridColumn = this.dataPos[i].x;
+            snake.style.gridRow = this.dataPos[i].y;
         }
     }
 }
 
 const utils =
 {
-    up: function() { snake.velocity.y = -1; snake.velocity.x = 0 },
-    right: function() { snake.velocity.x = 1; snake.velocity.y = 0 },
-    down: function() { snake.velocity.y = 1; snake.velocity.x = 0 },
-    left: function() { snake.velocity.x = -1; snake.velocity.y = 0 }
+    up: function()
+    {
+        if (snake.velocity.y <= 0)
+            snake.velocity.y = -1; snake.velocity.x = 0
+    },
+    right: function()
+    {
+        if (snake.velocity.x >= 0)
+            snake.velocity.x = 1; snake.velocity.y = 0
+    },
+    down: function()
+    {
+        if (snake.velocity.y >= 0)
+            snake.velocity.y = 1; snake.velocity.x = 0
+    },
+    left: function()
+    {
+        if (snake.velocity.x <= 0)
+            snake.velocity.x = -1; snake.velocity.y = 0
+    }
 }
+
+class GridArea
+{
+    constructor()
+    {
+        this.size = {width: 30, height: 30};
+    }
+}
+
+const gridArea = new GridArea();
 
 gameArea.innerHTML =
 `
@@ -60,11 +101,9 @@ document.addEventListener("keydown", (e) =>
         snake.move.left();
     else if (e.key == "Escape")
         clearInterval(interval)
-    else if (e.key == 'e')
-        interv
 });
 
 const interval = setInterval(() =>
 {
     snake.updatePosition();
-}, 50);
+}, 40);
